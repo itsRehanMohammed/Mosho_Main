7e6dc4c408f5b5f425f014c9867e31ded4578ee9
rehan.m <rehan.m@SG-M-51>
2023-09-29 17:07:41 +0530

    updates

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -10,10 +11,26 @@ const Footer = () => {
             <h1 className="ml-2 font-semibold font-spectral">The Mosho</h1>
           </div>
           <div className="flex flex-row my-3">
-            <img src="/assets/facebook-icon.png" alt="facebook" className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1" />
-            <img src="/assets/twitter-icon.png" alt="facebook" className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1" />
-            <img src="/assets/instagram-icon.png" alt="facebook" className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1" />
-            <img src="/assets/youtube-icon.png" alt="facebook" className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1" />
+            <img
+              src="/assets/facebook-icon.png"
+              alt="facebook"
+              className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1"
+            />
+            <img
+              src="/assets/twitter-icon.png"
+              alt="facebook"
+              className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1"
+            />
+            <img
+              src="/assets/instagram-icon.png"
+              alt="facebook"
+              className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1"
+            />
+            <img
+              src="/assets/youtube-icon.png"
+              alt="facebook"
+              className="border rounded-full w-[35px] hover:bg-[#ff492f] cursor-pointer p-1.5 mx-1"
+            />
           </div>
           <p>
             ©2021<span className="text-[#ff492f]"> The Mosho </span>
@@ -21,36 +38,67 @@ const Footer = () => {
         </div>
         <div className="grid md:grid-cols-4 grid-cols-2 md:mt-0 mt-10 w-full">
           <div className="flex flex-col items-start text-[14px] text-[#c9c9c9]">
-            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">Serivces</h1>
+            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">
+              Serivces
+            </h1>
             <div>
-              <h2 className="mt-1.5">Online Order</h2>
-              <h2 className="mt-1.5">Pre-Reservation</h2>
-              <h2 className="mt-1.5">24/7 Services</h2>
+              <h2 className="mt-1.5">
+                {" "}
+                <a href={`https://app.themosho.com/menu`}>Online Order </a>
+              </h2>
+              <h2 className="mt-1.5">
+                <a href={`https://app.themosho.com/contact`}>
+                  Pre-Reservation{" "}
+                </a>{" "}
+              </h2>
+              <h2 className="mt-1.5">
+                <Link to="/contact-us">24/7 Services</Link>
+              </h2>
             </div>
           </div>
           <div className="flex flex-col items-start text-[14px] text-[#c9c9c9]">
-            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">Quick Links</h1>
+            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">
+              Quick Links
+            </h1>
             <div>
-              <h2 className="mt-1.5">Restaurant</h2>
-              <h2 className="mt-1.5">Reviews</h2>
-              <h2 className="mt-1.5">Blogs</h2>
-              <h2 className="mt-1.5">Order Foods</h2>
+              <h2 className="mt-1.5">
+                <Link to="/restaurants">Restaurants</Link>
+              </h2>
+              <h2 className="mt-1.5">
+                {" "}
+                <a href={`https://app.themosho.com/menu`}> Order Foods </a>{" "}
+              </h2>
             </div>
           </div>
           <div className="flex flex-col items-start text-[14px] text-[#c9c9c9] md:mt-0 mt-5">
-            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">About</h1>
+            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">
+              {" "}
+              <Link to="/about">About</Link>{" "}
+            </h1>
             <div>
-              <h2 className="mt-1.5">Our Story</h2>
-              <h2 className="mt-1.5">Benifits</h2>
-              <h2 className="mt-1.5">Career</h2>
+              <h2 className="mt-1.5">
+                {" "}
+                <Link to="/about-us">Our Story </Link>{" "}
+              </h2>
+              <Link to="/offers">Offers</Link>
             </div>
           </div>
           <div className="flex flex-col items-start text-[14px] text-[#c9c9c9] md:mt-0 mt-5">
-            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">Help</h1>
+            <h1 className="mb-5 text-[20px] font-semibold font-spectral text-white">
+              Help
+            </h1>
             <div>
-              <h2 className="mt-1.5">Contact</h2>
-              <h2 className="mt-1.5">Support</h2>
-              <h2 className="mt-1.5">FAQ</h2>
+              <h2 className="mt-1.5">
+                {" "}
+                <Link to="/contact-us">Contact</Link>
+              </h2>
+              <h2 className="mt-1.5">
+                <a href={`https://app.themosho.com/contact`}>Support </a>{" "}
+              </h2>
+              <h2 className="mt-1.5">
+                {" "}
+                <Link to="/offers">FAQ</Link>
+              </h2>
             </div>
           </div>
         </div>
